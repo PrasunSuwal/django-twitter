98abd95d24c704a72ba2d9ca71536e60e69e6778
Prasun Suwal <prasun.suwal@gmail.com>
2025-05-10 10:04:49 +0545

    url change

--- learning/learning/urls.py
+++ learning/learning/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    path('', RedirectView.as_view(url='/tweet/', permanent=False)),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
